3836347dbfc134c010172f4035dcdf9c89c94017
jawed0216 <jawed0216@gmail.com>
2024-03-10 22:09:59 +0530

    fix

--- routes/miscellaneous.routes.js
+++ routes/miscellaneous.routes.js
@@ -15,16 +15,4 @@ router
 
 
 
-
-
-router.get("/", (req, res) => {
-	res.setHeader("Access-Control-Allow-Origin", "*")
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-});
-
-
-
 export default router;
