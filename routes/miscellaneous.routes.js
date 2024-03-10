da89270242acc513922fcadef190cc45109b57d3
jawed0216 <jawed0216@gmail.com>
2024-03-10 22:04:57 +0530

    fix

--- routes/miscellaneous.routes.js
+++ routes/miscellaneous.routes.js
@@ -13,4 +13,18 @@ router
   .route('/admin/stats/users')
   .get(isLoggedIn, authorizeRoles('ADMIN'), userStats);
 
+
+
+
+
+router.get("/", (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+
+
+
 export default router;
